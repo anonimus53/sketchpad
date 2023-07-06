7483fdef95fb5252f4e1e8d9ecb19df54d35610c
anonimus53 <futapas@yahoo.com>
2023-07-06 17:55:15 +0300

    finished

--- code.js
+++ code.js
@@ -1,27 +1,23 @@
-
-//let gridNum = 16;
-//let gridStr = gridNum.toString();
-//let gridNio = gridNum*gridNum;
-
-
-
-
+// Start by storing the container and initialize gridX = 0
 const container = document.getElementById('container');
-
+let gridX = 0;
+//Creates grid 16x16 if not num given
 function createGrid(num = 16){
 let str = num.toString();    
 container.style.gridTemplateColumns = 'repeat('+str+', 1fr)';
 container.style.gridTemplateRows = 'repeat('+str+', 1fr)';
 container.style.gap = '5px';
+gridX = num;
 }
 
-
+//Creates the divs to populate the grid of the DIV(id=container) 
 function createGridItems(num = 256){
  for(let i =0; i < num; i++){
    const item = document.createElement('div');
-   item.style.backgroundColor = 'white';
-   item.setAttribute('class', 'item');
-   item.textContent = '';
+   item.style.backgroundColor = 'white'; //All the divs(items) start white
+   item.setAttribute('class', 'item');//All divs have class = item 
+   item.textContent = '';//All divs are empty
+   //All divs have fucntion tha randomize their color when mouse passes by
     item.addEventListener('mouseover', function(){
       const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
       const r = randomBetween(0, 255);
@@ -30,20 +26,49 @@ function createGridItems(num = 256){
       const rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string
       item.style.backgroundColor = rgb;
     });
+    //Appends all divs to the grid
   container.appendChild(item);
  }
 }
 
-//first call
+//first call, Creation of default 16x16 grid
 createGrid();
 createGridItems();
 // first grid has been created
 
-//erase button, deletes grid and recreates it
+//display size var, current ontent is 16x16
+let currentSize = document.getElementById('size');
+//display size of grid
+function displaySiZe(gridsize = 16){
+  let strSize = gridsize.toString();
+  currentSize.textContent = strSize + 'x' + strSize;
+}
+
+//Erase button( deletes grid and recreates it new 16x16)
 const eraser = document.getElementById('eraser');
 eraser.addEventListener('click', function(){
    container.textContent = ' ';
-   createGridItems();
+   createGridItems(gridX*gridX);
+   displaySiZe(gridX); 
 })
 
-// I have to find a way to resize without breaking things...
+// I have to find a way to resize without breaking things
+//(new day)
+//gridX might help here...
+
+//Resize button(guess what it does XD)
+const resizeMe = document.getElementById('resize');
+resizeMe.addEventListener('click', function(){
+  gridX = prompt('Give me a Number between 1 and 50, cancel gives you the default grid 16x16');
+  if(gridX != null && gridX > 0 && gridX < 50){
+  container.textContent = ' ';
+  createGrid(gridX);
+  createGridItems(gridX*gridX);
+  displaySiZe(gridX);
+  }
+  else{
+    container.textContent = ' ';
+    createGrid();
+    createGridItems();
+  }
+})
